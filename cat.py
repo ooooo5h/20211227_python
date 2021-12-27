1cda42fca93bd41102a6119290cb294020520ccb
ooooo5h <ooooo5h91@gmail.com>
2021-12-27 11:54:33 +0900

    같은 이름의 파라미터 갯수가 다른 메소드 추가
    - 이전 메쏘드를 아예 날려버림

--- cat.py
+++ cat.py
@@ -13,3 +13,7 @@ class Cat(Animal):
         # 원래 동물로서 뛰어다니는 기능도 같이 실행하고싶다
         super().run()
         print('고양이가 뛰어다닙니다.')
+        
+    # # 사람과 놀아주는 기능 추가 작성
+    def play_with_human(self):
+        print(f'고양이가 사람과 놀아주는 두번째 메쏘드입니다.')
