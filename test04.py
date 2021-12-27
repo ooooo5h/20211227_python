79f8377376a81b8b03492ad9d1488f18263cb962
ooooo5h <ooooo5h91@gmail.com>
2021-12-27 16:02:37 +0900

    Dog클래스를 이용해서 변수/인스턴스 관계 체험
    - 변수끼리 대입하면, 같은 인스턴스를 보도록 처리됨

--- test04.py
+++ test04.py
@@ -1,3 +1,5 @@
+from datas.dog import Dog
+
 # 파이썬 제공 내장함수들 체험
 # 내장함수 : import 없이 진행
 
@@ -10,3 +12,19 @@ student_names = ['김범준', '김현희', '이승훈', '전은형']
 for i, name in enumerate(student_names):
     print(f'{i+1}번째 학생 : {name}')
 print('===============================')
+
+
+# 2. id / hex 체험
+
+dog1 = Dog('바둑이', 2015, '믹스견')
+dog2 = Dog('아롱이', 2010, '치와와')
+
+# 변수와 객체가 나눠져있다는 걸 체험해보자
+# dog1도 dog2와 같은 객체를 바라보게 된다
+dog1 = dog2
+
+# dog1의 이름을 -> '해피'로 변경
+dog1.name = '해피'
+
+dog1.print_dog_info()
+dog2.print_dog_info()
